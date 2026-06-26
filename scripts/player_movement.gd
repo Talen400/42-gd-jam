d895a50c7e229060f5c918f4bede150ee77d9f69
Talen400 <talengithub0@gmail.com>
2026-06-26 18:44:53 -0300

    feat: moviment of camera

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 1000.0
+const JUMP_VELOCITY = -800.0
 
 func death() -> void:
 	get_tree().reload_current_scene()
